3f5a8fcc75fe0efb5c40fb0ff30fd198563be3a0
Rafael Abuawad <rafael.abuawad@live.com>
2023-03-31 13:01:41 -0400

    First commit

---
+++ tokenvm/factory.go
@@ -0,0 +1,16 @@
+package vm
+
+import (
+	"github.com/ava-labs/avalanchego/utils/logging"
+	"github.com/ava-labs/avalanchego/vms"
+
+	"github.com/ava-labs/hypersdk/examples/tokenvm/controller"
+)
+
+var _ vms.Factory = &Factory{}
+
+type Factory struct{}
+
+func (*Factory) New(logging.Logger) (interface{}, error) {
+	return controller.New(), nil
+}
